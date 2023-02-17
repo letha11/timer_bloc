f67bb75ffa26ba7584635962ad401a6d2bb006bd
letha11 <ibkaanhar1@gmail.com>
2023-02-17 17:24:53 +0700

    feat: make the numpad disabled when timer start

--- lib/timer/view/widgets/num_pad.dart
+++ lib/timer/view/widgets/num_pad.dart
@@ -10,48 +10,59 @@ class NumPad extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 50.0),
-      child: Wrap(
-        spacing: 10,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        alignment: WrapAlignment.center,
-        children: [
-          for (var i = 0; i < 10; i++)
-            SizedBox(
-              width: 80,
-              height: 80,
-              child: FloatingNumberButton(
-                onPressed: () => context.read<TimerBloc>().add(TimerChanged((i + 1) == 10 ? 0 : i + 1)),
-                num: (i + 1) == 10 ? 0 : i + 1,
-              ),
-            ),
-          SizedBox(
-            width: 80,
-            height: 80,
-            child: FloatingActionButton(
-              child: const Icon(Icons.backspace),
-              onPressed: () => context.read<TimerBloc>().add(TimerChanged(null)),
-            ),
-          ),
-        ],
-      ),
+      child: BlocBuilder<TimerBloc, TimerState>(
+          buildWhen: (prevState, currState) => prevState.runtimeType != currState.runtimeType,
+          builder: (context, state) {
+            return Wrap(
+              spacing: 10,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              alignment: WrapAlignment.center,
+              children: [
+                for (var i = 0; i < 10; i++)
+                  SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: RoundedButton(
+                      onPressed: (state is TimerInitial)
+                          ? () => context.read<TimerBloc>().add(TimerChanged((i + 1) == 10 ? 0 : i + 1))
+                          : null,
+                      child: Text(
+                        ((i + 1) == 10 ? 0 : i + 1).toString(),
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ),
+                SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: RoundedButton(
+                    onPressed:
+                        (state is TimerInitial) ? () => context.read<TimerBloc>().add(const TimerChanged(null)) : null,
+                    child: const Icon(Icons.backspace),
+                  ),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
 
-class FloatingNumberButton extends StatelessWidget {
-  const FloatingNumberButton({Key? key, required this.num, required this.onPressed}) : super(key: key);
+class RoundedButton extends StatelessWidget {
+  const RoundedButton({Key? key, required this.child, required this.onPressed}) : super(key: key);
 
-  final int num;
-  final void Function() onPressed;
+  final Widget child;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
+    return ElevatedButton(
       onPressed: onPressed,
-      child: Text(
-        num.toString(),
-        style: Theme.of(context).textTheme.displaySmall?.copyWith(color: Colors.white),
+      style: ElevatedButton.styleFrom(
+
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
       ),
+      child: child,
     );
   }
 }
